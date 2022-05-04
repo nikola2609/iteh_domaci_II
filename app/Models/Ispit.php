9b5084afe85142875a506a7e784c23649ca6f404
nikola2609 <nikolalazovic26@gmail.com>
2022-05-04 19:25:27 +0200

    modeli povezani

--- app/Models/Ispit.php
+++ app/Models/Ispit.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Ispit extends Model
 {
     use HasFactory;
+    protected $guarded=[''];
+
+    public function prijava(){
+        return $this->hasMany(Prijava::class);
+    }
 }
--- app/Models/Prijava.php
+++ app/Models/Prijava.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Prijava extends Model
 {
     use HasFactory;
+    protected $guarded=[''];
+
+    public function ispit(){
+        return $this->belongsTo(Ispit::class);
+    }
+    public function student(){
+        return $this->belongsTo(Student::class);
+    }
 }
